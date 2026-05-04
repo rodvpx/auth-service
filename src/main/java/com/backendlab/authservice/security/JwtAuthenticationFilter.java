f5500f6f75ae27be5be2cfd69e68b5d5194f2aab
Rodrigo Simão <rodrigoip435@gmail.com>
2026-05-04 19:06:25 -0300

    refactor(security): adiciona detalhes da autenticação no JwtAuthenticationFilter

--- src/main/java/com/backendlab/authservice/security/JwtAuthenticationFilter.java
+++ src/main/java/com/backendlab/authservice/security/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.backendlab.authservice.security;
 
-import com.backendlab.authservice.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -42,8 +42,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             var userDetails = userDetailsService.loadUserByUsername(email);
 
             if (jwtService.isTokenValid(token)) {
-                var authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
+
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities()
+                        );
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
