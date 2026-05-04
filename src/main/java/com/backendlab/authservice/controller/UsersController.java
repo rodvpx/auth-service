695f5102433e4b611ae092827f9d915cc4ff8f86
Rodrigo Simão <rodrigoip435@gmail.com>
2026-05-04 16:29:42 -0300

    feat(controller): adiciona endpoint de login de usuário em UsersController

--- src/main/java/com/backendlab/authservice/controller/UsersController.java
+++ src/main/java/com/backendlab/authservice/controller/UsersController.java
@@ -1,7 +1,9 @@
 package com.backendlab.authservice.controller;
 
+import com.backendlab.authservice.dto.LoginRequest;
 import com.backendlab.authservice.dto.RegisterRequest;
 import com.backendlab.authservice.entity.User;
+import com.backendlab.authservice.service.AuthService;
 import com.backendlab.authservice.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +15,17 @@ import org.springframework.web.bind.annotation.*;
 public class UsersController {
 
     private final UserService service;
+    private final AuthService authService;
 
     @PostMapping("/register")
     public User register(@RequestBody @Valid RegisterRequest request) {
 
         return service.register(request);
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestBody @Valid LoginRequest request) {
+
+        return authService.login(request);
     }
 }
