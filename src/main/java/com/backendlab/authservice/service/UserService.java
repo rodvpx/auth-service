bf3c7924001377739fd73611309a6d5775032497
Rodrigo Simão <rodrigoip435@gmail.com>
2026-05-04 16:05:45 -0300

    feat(service): adiciona UserService com lógica de registro de usuário

---
+++ src/main/java/com/backendlab/authservice/service/UserService.java
@@ -0,0 +1,34 @@
+package com.backendlab.authservice.service;
+
+import com.backendlab.authservice.dto.RegisterRequest;
+import com.backendlab.authservice.entity.Role;
+import com.backendlab.authservice.entity.User;
+import com.backendlab.authservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+
+    public User register(RegisterRequest request) {
+
+        if (repository.existsByEmail(request.email())) {
+            throw new RuntimeException("Email já cadastrado");
+        }
+
+        User user = User.builder()
+                .name(request.name())
+                .email(request.email())
+                .password(request.password())
+                .createdAt(LocalDateTime.now())
+                .role(Role.USER)
+                .build();
+
+        return repository.save(user);
+    }
+}
